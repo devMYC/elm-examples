ec4309147ac44cbfda6c5ab3e9dc1671b75461b7
Martin <mycha0@hotmail.com>
2017-09-18 14:27:57 +0800

    code formatting

--- src/clock.elm
+++ src/clock.elm
@@ -1,3 +1,5 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
@@ -6,48 +8,52 @@ import Time exposing (..)
 
 
 main =
-  program
-    { init = init
-    , update = update
-    , view = view
-    , subscriptions = subscriptions
-    }
+    program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
 
 
-type alias Model = Time
+type alias Model =
+    Time
 
 
 init : ( Model, Cmd Msg )
-init = ( 0, perform Tick now )
+init =
+    ( 0, perform Tick now )
 
 
 type Msg
-  = Tick Time
+    = Tick Time
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Tick timeNow ->
-      ( timeNow, Cmd.none )
+    case msg of
+        Tick timeNow ->
+            ( timeNow, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  every second Tick
+    every second Tick
 
 
 view : Model -> Html Msg
 view model =
-  let
-    angle
-      = turns ( model |> inMinutes )
+    let
+        angle =
+            turns (model |> inMinutes)
 
-    secHandX = toString ( 50 + 40 * cos angle )
-    secHandY = toString ( 50 + 40 * sin angle )
+        secHandX =
+            toString (50 + 40 * cos angle)
 
-  in
-    svg [ viewBox "0 0 100 100", width "300px" ]
-      [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-      , line [ x1 "50", y1 "50", x2 secHandX, y2 secHandY, stroke "#000000" ] []
-      ]
+        secHandY =
+            toString (50 + 40 * sin angle)
+    in
+        svg [ viewBox "0 0 100 100", width "300px" ]
+            [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+            , line [ x1 "50", y1 "50", x2 secHandX, y2 secHandY, stroke "#000000" ] []
+            ]
--- src/counter.elm
+++ src/counter.elm
@@ -1,3 +1,5 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
@@ -5,83 +7,91 @@ import Process exposing (..)
 import Task exposing (..)
 import Time exposing (..)
 
+
 main =
-  program
-    { init = init
-    , update = update
-    , view = view
-    , subscriptions = always Sub.none
-    }
+    program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = always Sub.none
+        }
 
 
-type alias Model = Int
+type alias Model =
+    Int
+
 
 initialModel : Model
-initialModel = 0
+initialModel =
+    0
+
 
 type Msg
-  = Increment
-  | Decrement
-  | IncrementIfOdd
-  | Reset
-  | IncrementAsync
+    = Increment
+    | Decrement
+    | IncrementIfOdd
+    | Reset
+    | IncrementAsync
 
 
 init : ( Model, Cmd Msg )
-init = ( initialModel, Cmd.none )
+init =
+    ( initialModel, Cmd.none )
 
 
 asyncIncrement : Cmd Msg
 asyncIncrement =
-  sleep second
-  |> perform (\_ -> Increment)
+    sleep second
+        |> perform (\_ -> Increment)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Increment ->
-      ( model + 1, Cmd.none )
+    case msg of
+        Increment ->
+            ( model + 1, Cmd.none )
 
-    Decrement ->
-      ( model - 1, Cmd.none )
+        Decrement ->
+            ( model - 1, Cmd.none )
 
-    IncrementIfOdd ->
-      if model % 2 == 0 then
-        ( model, Cmd.none )
-      else
-        ( model + 1, Cmd.none )
+        IncrementIfOdd ->
+            if model % 2 == 0 then
+                ( model, Cmd.none )
+            else
+                ( model + 1, Cmd.none )
 
-    Reset -> ( 0, Cmd.none )
+        Reset ->
+            ( 0, Cmd.none )
 
-    IncrementAsync -> ( model, asyncIncrement )
+        IncrementAsync ->
+            ( model, asyncIncrement )
 
 
 containerStyle : Attribute Msg
 containerStyle =
-  style
-    [ ("margin-left", "20px") ]
+    style
+        [ ( "margin-left", "20px" ) ]
+
 
 headerStyle : Attribute Msg
 headerStyle =
-  style
-    [ ("color", "red")
-    , ("font-family", "Consolas")
-    , ("font-size", "10em")
-    , ("font-weight", "bold")
-    ]
-
+    style
+        [ ( "color", "red" )
+        , ( "font-family", "Consolas" )
+        , ( "font-size", "10em" )
+        , ( "font-weight", "bold" )
+        ]
 
 
 view : Model -> Html Msg
 view model =
-  div [ containerStyle ]
-    [ h1 [ headerStyle ] [ text (toString model) ]
-    , div [ containerStyle ]
-        [ button [ onClick Increment ] [ text "+" ]
-        , button [ onClick Decrement ] [ text "-" ]
-        , button [ onClick IncrementIfOdd ] [ text "+ if odd" ]
-        , button [ onClick IncrementAsync ] [ text "+ async" ]
-        , button [ onClick Reset ] [ text "reset" ]
+    div [ containerStyle ]
+        [ h1 [ headerStyle ] [ text (toString model) ]
+        , div [ containerStyle ]
+            [ button [ onClick Increment ] [ text "+" ]
+            , button [ onClick Decrement ] [ text "-" ]
+            , button [ onClick IncrementIfOdd ] [ text "+ if odd" ]
+            , button [ onClick IncrementAsync ] [ text "+ async" ]
+            , button [ onClick Reset ] [ text "reset" ]
+            ]
         ]
-    ]
